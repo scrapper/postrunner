e3f780b87b99bcabc0017c8be86375c667489f02
Chris Schlaeger <chris@linux.com>
2014-08-15 20:01:00 +0200

    All files are now explicitely GNU GPLv2 licensed.

--- lib/postrunner.rb
+++ lib/postrunner.rb
@@ -1,3 +1,15 @@
+#!/usr/bin/env ruby -w
+# encoding: UTF-8
+#
+# = postrunner.rb -- PostRunner - Manage the data from your Garmin sport devices.
+#
+# Copyright (c) 2014 by Chris Schlaeger <cs@taskjuggler.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+
 $:.unshift(File.join(File.dirname(__FILE__), '..', '..', 'fit4ruby', 'lib'))
 $:.unshift(File.dirname(__FILE__))
 
--- lib/postrunner/ActivitiesDB.rb
+++ lib/postrunner/ActivitiesDB.rb
@@ -1,3 +1,15 @@
+#!/usr/bin/env ruby -w
+# encoding: UTF-8
+#
+# = ActivitiesDB.rb -- PostRunner - Manage the data from your Garmin sport devices.
+#
+# Copyright (c) 2014 by Chris Schlaeger <cs@taskjuggler.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+
 require 'fileutils'
 require 'yaml'
 
