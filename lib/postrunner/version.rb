2d19802c556349a770511a283b1af072f200a52e
Chris Schlaeger <chris@linux.com>
2016-03-01 20:58:39 +0100

    Bump version to 0.2.0

--- lib/postrunner/version.rb
+++ lib/postrunner/version.rb
@@ -3,7 +3,7 @@
 #
 # = version.rb -- PostRunner - Manage the data from your Garmin sport devices.
 #
-# Copyright (c) 2014 by Chris Schlaeger <cs@taskjuggler.org>
+# Copyright (c) 2014, 2015, 2016 by Chris Schlaeger <cs@taskjuggler.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of version 2 of the GNU General Public License as
@@ -11,5 +11,5 @@
 #
 
 module PostRunner
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 end
