24f945d85672e9a6b0ff7be3450fda6939b1655d
Chris Schlaeger <chris@linux.com>
2020-07-26 15:43:44 +0200

    Set default log level back to INFO

--- lib/postrunner/Log.rb
+++ lib/postrunner/Log.rb
@@ -19,7 +19,7 @@ module PostRunner
   Log.formatter = proc { |severity, datetime, progname, msg|
     "#{severity == Logger::INFO ? '' : "#{severity}:"} #{msg}\n"
   }
-  Log.level = Logger::WARN
+  Log.level = Logger::INFO
 
 end
 
